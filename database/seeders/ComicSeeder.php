50d1304d6753167dcee7bcfa51d79e11afd81a7f
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-29 15:56:57 +0200

    seeder fixed

--- database/seeders/ComicSeeder.php
+++ database/seeders/ComicSeeder.php
@@ -14,20 +14,12 @@ class ComicSeeder extends Seeder
     public function run(): void
     {
 
+        $comics = config('comics');
 
-
-        $new_comic = new Comic();
-
-        $new_comic->title = 'Catwoman Vol. 1: Copycats';
-        $new_comic->description = "Following her near-miss wedding with Batman, Catwoman hits the streets to expose a copycat who’s pulling heists around Gotham City. As Selina cracks the whip on her former criminal cohorts, she’s attracting unwanted attention from one of Gotham’s most dangerous groups. The mob? Nope. Try the GCPD. And as if the Bat-Bride didn’t have enough problems, don’t miss the debut of an all-new villain determined to make trouble for all nine of Selina’s lives. Collects issues #1-6 of Catwoman’s new series.";
-        $new_comic->thumb = "https://imgs.search.brave.com/bf_FlGnymAgmvweY8YiLAcyy600JdSMl_jHpdR3t5ro/rs:fit:612:612:1/g:ce/aHR0cHM6Ly9pNS53/YWxtYXJ0aW1hZ2Vz/LmNvbS9hc3IvNTA5/M2IxOTEtZTkzZS00/NTJjLThhYTQtYTEw/ZDY0ZTcxN2E5LjA5/N2UwZWM5NDgzYzky/OGExMjY4Nzk1Nzk2/NTliZWNjLmpwZWc_/b2RuV2lkdGg9NjEy/Jm9kbkhlaWdodD02/MTImb2RuQmc9ZmZm/ZmZm";
-        $new_comic->price = "$16.99";
-        $new_comic->series = "Catwoman";
-        $new_comic->sale_date = "2019-04-10";
-        $new_comic->type = "graphic novel";
-        $new_comic->artists = "Fernando Blanco";
-        $new_comic->writers = "Joëlle Jones";
-
-        $new_comic->save();
+        foreach ($comics as $comic) {
+            $new_comic = new Comic();
+            $new_comic->fill($comic);
+            $new_comic->save();
+        }
     }
 }
