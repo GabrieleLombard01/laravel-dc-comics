4895308285ea06d4260c32c75606eabb809b54f4
GabrieleLombard01 <gabriellombardo2001@gmail.com>
2023-08-30 19:18:28 +0200

    contents add to show

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -3,25 +3,113 @@
 @section('title', $comic->title)
 
 @section('main-content')
-    <div class="container-lg position-relative">
+    <div class="show_content">
 
-        <!-- contents: -->
-        <div id="products" class="container card-deck">
 
-            {{-- TO DO = GRAFICA COMIC --}}
-            {{-- video 75 min. 59.29 --}}
-
-            @foreach ($comics as $comic)
-                <div class="card bg-dark m-2" style="width: 18rem;">
-                    <img src="{{ $comic->thumb }}" class="card-img-top" alt="{{ $comic->title }}">
-                    <div class="card-body">
-                        <h5 class="text-white card-title">{{ $comic->title }}</h5>
-                    </div>
-                </div>
-            @endforeach
-
+        <!--img view-->
+        <div class="blue_box">
+            <div class="container-lg">
+                <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
+                <button class="btn btn-success"><a class="text-white" href="{{ route('comics.index') }}">Back to
+                        comics</a></button>
+            </div>
 
         </div>
+        <div class="container-lg position-relative">
 
+            <!-- contents: -->
+            <div id="products" class="container ps-3 pt-4">
+
+                <h1>{{ $comic->title }}</h1>
+
+                <div class="row">
+                    <!--description-->
+                    <div class="col-8">
+                        <div class="green_box">
+
+                            <div class="col-8 ps-2">U.S Price:
+                                <span class="ps-1 fw-bold text-white">{{ $comic->price }}</span>
+                            </div>
+                            <div class="col-2">Avaible</div>
+                            <div class="col-2">Check Availibility</div>
+
+                        </div>
+                        <p class="pt-3">{{ $comic->description }}</p>
+                    </div>
+
+                    <!--ADV-->
+                    <div class="col-3 pb-4">
+                        <h3 class="text-end">ADVERTISEMENT</h3>
+                        <img src="{{ asset('images/adv.jpg') }}" alt="ADV">
+                    </div>
+                </div>
+
+                <!--TABLES-->
+                <div class="row pb-5">
+                    <div class="col-6">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Art by</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{{ $comic->artists }}</td>
+                                </tr>
+                            </tbody>
+                            <thead>
+                                <tr>
+                                    <th scope="col">Written by</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{{ $comic->writers }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div class="col-6">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Series</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{{ $comic->series }}</td>
+                                </tr>
+                            </tbody>
+                            <thead>
+                                <tr>
+                                    <th scope="col">On sale Date</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{{ $comic->sale_date }}</td>
+                                </tr>
+                            </tbody>
+                            <thead>
+                                <tr>
+                                    <th scope="col">Price</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>{{ $comic->price }}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+
+
+            </div>
+        </div>
     </div>
 @endsection
